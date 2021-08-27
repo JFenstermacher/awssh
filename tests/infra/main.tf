353bb84cbff6cd1ad5729c35dc5cdc3359432aca
Jonathan Fenstermacher <jfenstermacher123@gmail.com>
2021-08-27 15:36:37 -0400

    Key pair generated and allows access

--- tests/infra/main.tf
+++ tests/infra/main.tf
@@ -4,7 +4,26 @@ terraform {
       source  = "hashicorp/aws"
       version = "3.56.0"
     }
+    tls  = {
+      source = "hashicorp/tls"
+      version = "~>3.1.0"
+    }
   }
+}
+
+provider "aws" {}
+provider "tls" {}
+
+locals {
+  prefix = "awssh-test"
+}
+
+data "http" "ip" {
+  url = "http://icanhazip.com"
+}
+
+data "aws_vpc" "default" {
+  default = true
 }
 
 data "aws_ami" "amazon_linux" {
@@ -12,18 +31,56 @@ data "aws_ami" "amazon_linux" {
   owners      = ["amazon"]
 
   filter {
-    name = "name"
+    name   = "name"
     values = ["amzn2-ami-hvm*"]
   }
 }
 
-resource "aws_instance" "test" {
-  count = 3
+resource "tls_private_key" "ssh" {
+  algorithm = "RSA"
+  rsa_bits  = "4096"
+}
 
-  ami = data.aws_ami.amazon_linux.id
-  instance_type = "t2.micro"
+resource "local_file" "private_key" {
+  content = tls_private_key.ssh.private_key_pem
+  filename = "${local.prefix}.pem"
+  file_permission = "0600"
+}
+
+resource "aws_key_pair" "test" {
+  key_name_prefix = local.prefix
+  public_key      = tls_private_key.ssh.public_key_openssh
+}
+
+resource "aws_security_group" "test" {
+  name_prefix = local.prefix
+  vpc_id = data.aws_vpc.default.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${chomp(data.http.ip.body)}/32"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "test" {
+  count = 1
+
+  ami                         = data.aws_ami.amazon_linux.id
+  instance_type               = "t2.micro"
+  associate_public_ip_address = true
+  key_name                    = aws_key_pair.test.key_name
+  vpc_security_group_ids      = [aws_security_group.test.id]
 
   tags = {
-    Name = "test-server-${count.index}"
+    Name = "prefix-${count.index}"
   }
 }
