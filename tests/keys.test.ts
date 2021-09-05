6143f032df95e1bf255489da3135f2eb92caaf58
Jonathan Fenstermacher <jfenstermacher123@gmail.com>
2021-09-04 20:36:29 -0400

    more keys tests

--- tests/keys.test.ts
+++ tests/keys.test.ts
@@ -1,38 +1,73 @@
-import { join } from "https://deno.land/std@0.101.0/node/path.ts";
+import { createHash } from "https://deno.land/std@0.101.0/hash/mod.ts";
+import { join, resolve } from "https://deno.land/std@0.101.0/node/path.ts";
 import { assertEquals } from "https://deno.land/std@0.106.0/testing/asserts.ts";
 import { Configuration } from "../libs/config.ts";
 import { Keys } from "../libs/keys.ts";
-import { Key } from "../libs/types.ts";
+import { FormattedInstance, Key } from "../libs/types.ts";
 import { checkFileExists } from "./utilities.ts";
 
-const TEST_KEY: Key = {
-  name: "test-key",
-  location: Deno.cwd(),
-  hash: "test-hash",
+const INDEX_FILE = join(Deno.cwd(), "index.ts");
+
+const TEST_KEYS: Key[] = [
+  {
+    name: "test-key",
+    location: join(Deno.cwd(), "index.ts"),
+    hash: await Deno.readTextFile(INDEX_FILE)
+      .then((data) => createHash("md5").update(data).toString()),
+  },
+  {
+    name: "test-key-1",
+    location: join(Deno.cwd(), "test-key-1.pem"),
+    hash: "test-hash",
+  },
+  {
+    name: "test-key-2",
+    location: join(Deno.cwd(), "test-key-2.pem"),
+    hash: "test-hash",
+  },
+];
+
+const TEST_INSTANCE: FormattedInstance = {
+  ImageId: "ami-123",
+  InstanceId: "i-abc",
+  InstanceType: "t2.micro",
+  KeyName: "awssh-test",
+  PrivateIpAddress: "100.100.100.100",
+  PublicIpAddress: "999.100.100.100",
+  SubnetId: "subnet-abc",
+  VpcId: "vpc-abc",
+  SSMEnabled: false,
+  State: "running",
+  Tags: { Name: "prefix-0" },
 };
 
-Deno.test("Can save and wipe cache", async () => {
+Deno.test("Can write, check, and wipe cache", async () => {
   const cacheDir = join(Deno.cwd(), "tests", "keycache");
   const { env } = Deno;
 
   env.set("XDG_CACHE_HOME", cacheDir);
   const keysClass = new Keys(Configuration.defaults, {});
 
-  const instanceId = "test-instance";
-  await keysClass.save(instanceId, TEST_KEY);
+  // Saving Key
+  const instanceId = "i-abc";
+  await keysClass.save(instanceId, TEST_KEYS[0]);
 
   const cachePath = join(cacheDir, "awssh", "keys.yaml");
   const exists = await checkFileExists(cachePath);
-
   assertEquals(exists, true);
 
   const expected = {
-    [instanceId]: TEST_KEY,
+    [instanceId]: TEST_KEYS[0],
   };
 
   let cache = await keysClass.read();
   assertEquals(cache, expected);
 
+  // Checking Cache
+  const cachedResult = await keysClass.checkCache(TEST_INSTANCE);
+  assertEquals(cachedResult, TEST_KEYS[0]);
+
+  // Wiping of Cache
   await Keys.wipe();
 
   cache = await keysClass.read();
@@ -49,4 +84,31 @@ Deno.test("Non-key files throw failures", async () => {
   const output = await keysClass.isFileKey(indexFile);
 
   assertEquals(output, false);
+});
+
+Deno.test("Can detect key by name", () => {
+  const keysClass = new Keys(Configuration.defaults, {});
+
+  const keyName = "test-key-1";
+
+  const expected = TEST_KEYS[1];
+  const result = keysClass.detectKeyByName(keyName, TEST_KEYS);
+
+  assertEquals(result, expected);
+});
+
+Deno.test("Can format a key file", async () => {
+  const keysClass = new Keys(Configuration.defaults, {});
+
+  const indexFile = join(Deno.cwd(), "index.ts");
+
+  const expected: Key = {
+    name: "index",
+    location: resolve(indexFile),
+    hash: await keysClass.hash(indexFile) as string,
+  };
+
+  const result = await keysClass.format(indexFile);
+
+  assertEquals(result, expected);
 });
